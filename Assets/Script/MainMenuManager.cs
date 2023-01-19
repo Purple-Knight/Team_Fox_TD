3026b8efd22b0c22e0cbb558e4de2d3cdc1f3f13
Franci Algera <algera.franci@gmail.com>
2023-01-19 19:48:58 +0100

    Main menu added

---
+++ Assets/Script/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
