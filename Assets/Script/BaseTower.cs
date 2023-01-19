0a5494b20a84af7d17e9fb4c3cbbe282862909a4
luca.schifano005@gmail.com <luca.schifano005@gmail.com>
2023-01-19 12:21:43 +0100

    Enemies Detection

--- Assets/Script/BaseTower.cs
+++ Assets/Script/BaseTower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,47 @@ public class BaseTower : MonoBehaviour
     [SerializeField] private float coolDownBtwShot;
     private float actualCoolDownBtwShot;
     [SerializeField] private float attackPower;
-    [SerializeField] private List<GameObject> enemisList;
+    [SerializeField] private List<GameObject> enemisList = new List<GameObject>();
     
+    private CircleCollider2D rangeCollider2D;
+
+    protected void Start()
+    {
+        rangeCollider2D = gameObject.AddComponent<CircleCollider2D>();
+        rangeCollider2D.radius = radius;
+        rangeCollider2D.offset = Vector2.zero;
+        rangeCollider2D.isTrigger = true;
+    }
     
+    protected void SetTarget()
+    {
+        
+    }
+
+    protected virtual void Shoot()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            enemisList.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            enemisList.Remove(other.gameObject);
+        }
+    }
 }
